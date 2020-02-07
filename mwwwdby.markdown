5c6dd2d6b96d5e2841a30ca9d0cd1ba60cbc6e6e
saadbintariq444 <60772702+saadbintariq444@users.noreply.github.com>
2020-02-07 12:50:58 +0500

    Add files via upload

---
+++ mwwwdby.markdown
@@ -0,0 +1,7 @@
+MWwWdby
+-------
+
+
+A [Pen](https://codepen.io/saad444/pen/MWwWdby) by [Saad Bin Tariq](https://codepen.io/saad444) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/saad444/pen/MWwWdby/license).
---
+++ script.js
@@ -0,0 +1,9 @@
+button = document.getElementById("apply-button");
+
+button.onmouseover = function() {
+    this.style.backgroundColor = "#8b0000";
+  }
+
+button.onmouseout = function() {
+    this.style.backgroundColor = "#BB2026";
+  }
